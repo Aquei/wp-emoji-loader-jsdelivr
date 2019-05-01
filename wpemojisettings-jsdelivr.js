97c309f50815177eda24b89006df8583ee6ed155
aquei <Aquei@users.noreply.github.com>
2019-05-02 07:37:49 +0900

    find wp version

--- wpemojisettings-jsdelivr.js
+++ wpemojisettings-jsdelivr.js
@@ -1,1 +1,16 @@
-window._wpemojiSettings = {"baseUrl":"https://cdn.jsdelivr.net/gh/twitter/twemoji/2/72x72/","ext":".png","svgUrl":"https://cdn.jsdelivr.net/npm/twemoji/2/svg/","svgExt":".svg","source":{"concatemoji":"https://cdn.jsdelivr.net/gh/WordPress/WordPress/wp-includes/js/wp-emoji-release.min.js"}};
+(function(w, d){
+	"use strict";
+	var elm = d.currentScript || d.querySelector("[data-wpv]"),
+		regex = /^\d+(?:\.\d+){1,2}$/,
+		v;
+
+	if(elm){
+		v = elm.getAttribute("data-wpv");
+		if(regex.test(v)){
+			v = "@" + v;
+		}else{
+			v = "";
+		}
+	}
+w._wpemojiSettings = {"baseUrl":"https://cdn.jsdelivr.net/gh/twitter/twemoji/2/72x72/","ext":".png","svgUrl":"https://cdn.jsdelivr.net/npm/twemoji/2/svg/","svgExt":".svg","source":{"concatemoji":"https://cdn.jsdelivr.net/gh/WordPress/WordPress" + v + "/wp-includes/js/wp-emoji-release.min.js"}};
+})(window, document);
