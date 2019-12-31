5460e377dfd5ab75651a055975009f5c34db5306
aquei <Aquei@users.noreply.github.com>
2020-01-01 08:20:03 +0900

    support twemoji v12

--- wpemojisettings-jsdelivr.js
+++ wpemojisettings-jsdelivr.js
@@ -12,5 +12,5 @@
 			v = "";
 		}
 	}
-w._wpemojiSettings = {"baseUrl":"https://cdn.jsdelivr.net/gh/twitter/twemoji/2/72x72/","ext":".png","svgUrl":"https://cdn.jsdelivr.net/npm/twemoji/2/svg/","svgExt":".svg","source":{"concatemoji":"https://cdn.jsdelivr.net/gh/WordPress/WordPress" + v + "/wp-includes/js/wp-emoji-release.min.js"}};
+w._wpemojiSettings = {"baseUrl":"https://cdn.jsdelivr.net/gh/twitter/twemoji/assets/72x72/","ext":".png","svgUrl":"https://cdn.jsdelivr.net/gh/twitter/twemoji/assets/svg/","svgExt":".svg","source":{"concatemoji":"https://cdn.jsdelivr.net/gh/WordPress/WordPress" + v + "/wp-includes/js/wp-emoji-release.min.js"}};
 })(window, document);
